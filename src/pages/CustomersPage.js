ff6716e8a6d6aa85f493d233bd12d97ea7c42a87
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-07-31 01:20:58 +0530

    created empty customers message ui

--- src/pages/CustomersPage.js
+++ src/pages/CustomersPage.js
@@ -39,27 +39,20 @@ const useStyles = makeStyles((theme) => ({
 
 function CustomersPage() {
     const classes = useStyles()
+    const [customers, setCustomers] = useState(Array(20).fill(1))
     const [openAddCustomerDialog, setOpenAddCustomerDialog] = useState(false)
     return (
         <>
             <Grid className={classes.customersPageContainer}>
                 <Grid style={{ width: '100%', height: '4rem', backgroundColor: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid lightgray', borderTopLeftRadius: 'inherit', borderTopRightRadius: 'inherit' }}>
-                    <Box style={{marginLeft: '5px', fontSize: '20px', fontFamily:'fantasy'}}>Pixel6 CRUD Application</Box>
-                    <Button className={classes.activeButton} onClick={()=>setOpenAddCustomerDialog(true)}>Add Customer</Button>
+                    <Box style={{ marginLeft: '5px', fontSize: '20px', fontFamily: 'fantasy' }}>Pixel6 CRUD Application</Box>
+                    <Button className={classes.activeButton} onClick={() => setOpenAddCustomerDialog(true)}>Add Customer</Button>
                 </Grid>
-                <Grid style={{ width: '100%', height: 'calc(100% - 4rem)', backgroundColor: 'white', display: 'flex', flexDirection: 'column', padding: '0.25rem', borderBottomLeftRadius:'inherit', borderBottomRightRadius: 'inherit' }}>
-                    <Grid style={{ backgroundColor: 'gray', display: 'flex', alignItems: 'center', padding: '15px 0px', color: 'white', borderTopLeftRadius: '5px', borderTopRightRadius: '5px', position: 'sticky', paddingRight: '4px' }}>
-                        <Box style={{ width: '5%' }} className={classes.column}>Sr No</Box>
-                        <Box style={{ width: '20%' }} className={classes.column}>Customer Name</Box>
-                        <Box style={{ width: '20%' }} className={classes.column}>Email</Box>
-                        <Box style={{ width: '15%' }} className={classes.column}>Mobile No</Box>
-                        <Box style={{ width: '15%' }} className={classes.column}>PAN No</Box>
-                        <Box style={{ width: '30%', border: 'none' }} className={classes.column}>Address</Box>
-                    </Grid>
-                    <Grid style={{ height: '100%', width: '100%', overflowY: 'auto', border: '1px solid gray' }}>
-                        {
-                            Array(20).fill(1).map((item, index) => (
-                                <Grid style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 0px', borderBottom: '1px solid lightgrey', height: '4rem', overflow: 'hidden' }} key={index}>
+                <Grid style={{ width: '100%', height: 'calc(100% - 4rem)', backgroundColor: 'white', display: 'flex', flexDirection: 'column', padding: '0.25rem', borderBottomLeftRadius: 'inherit', borderBottomRightRadius: 'inherit' }}>
+                    {
+                        customers.length > 0 ?
+                            <>
+                                <Grid style={{ backgroundColor: 'gray', display: 'flex', alignItems: 'center', padding: '15px 0px', color: 'white', borderTopLeftRadius: '5px', borderTopRightRadius: '5px', position: 'sticky', paddingRight: '4px' }}>
                                     <Box style={{ width: '5%' }} className={classes.column}>Sr No</Box>
                                     <Box style={{ width: '20%' }} className={classes.column}>Customer Name</Box>
                                     <Box style={{ width: '20%' }} className={classes.column}>Email</Box>
@@ -67,12 +60,29 @@ function CustomersPage() {
                                     <Box style={{ width: '15%' }} className={classes.column}>PAN No</Box>
                                     <Box style={{ width: '30%', border: 'none' }} className={classes.column}>Address</Box>
                                 </Grid>
-                            ))
-                        }
-                    </Grid>
+                                <Grid style={{ height: '100%', width: '100%', overflowY: 'auto', border: '1px solid gray' }}>
+                                    {
+                                        customers.map((item, index) => (
+                                            <Grid style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 0px', borderBottom: '1px solid lightgrey', height: '4rem', overflow: 'hidden' }} key={index}>
+                                                <Box style={{ width: '5%' }} className={classes.column}>Sr No</Box>
+                                                <Box style={{ width: '20%' }} className={classes.column}>Customer Name</Box>
+                                                <Box style={{ width: '20%' }} className={classes.column}>Email</Box>
+                                                <Box style={{ width: '15%' }} className={classes.column}>Mobile No</Box>
+                                                <Box style={{ width: '15%' }} className={classes.column}>PAN No</Box>
+                                                <Box style={{ width: '30%', border: 'none' }} className={classes.column}>Address</Box>
+                                            </Grid>
+                                        ))
+                                    }
+                                </Grid>
+                            </> :
+                            <Grid style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                <Box style={{ fontSize: '30px', fontWeight: 'bold', color: 'gray' }}>No Customers Available</Box>
+                            </Grid>
+                    }
+
                 </Grid>
                 {
-                    openAddCustomerDialog && <AddCustomerDialog open={openAddCustomerDialog} onClose={()=>setOpenAddCustomerDialog(false)}/>
+                    openAddCustomerDialog && <AddCustomerDialog open={openAddCustomerDialog} onClose={() => setOpenAddCustomerDialog(false)} />
                 }
             </Grid>
         </>
