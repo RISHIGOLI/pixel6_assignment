73db233ce39386b7ce042cc1772e8dcbabb0de69
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-08-01 15:12:21 +0530

    created proper entry messsage

--- src/components/EntryMessageDialog.js
+++ src/components/EntryMessageDialog.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Box, Grid } from '@mui/material'
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -34,10 +35,25 @@ export function EntryMessageDialog({ open, onClose }) {
             keepMounted
             aria-describedby="alert-dialog-slide-description"
         >
-            <DialogTitle style={{display: 'flex', justifyContent: 'center'}}>{'Pixel6 React Assingment'}</DialogTitle>
-            <DialogContent style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+            <DialogTitle style={{ display: 'flex', justifyContent: 'center' }}>{'Pixel6 React Assingment'}</DialogTitle>
+            <DialogContent style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
                 <DialogContentText id="alert-dialog-slide-description">
-                    This is developed to show case my skills related to frontend or espacially in reactjs
+                    <Box style={{ fontWeight: 'bold' }}>Welcome to My React Assignment!</Box>
+
+                    <Box>Hello there! 🌟</Box>
+                    <Grid style={{ width: '100%', textAlign: 'justify' }}>
+                        <Box style={{margin: '10px 0px'}}>Thank you for taking the time to review my ReactJS assignment. I’ve created a dynamic and interactive application to showcase my frontend development skills.</Box>
+
+                        <Box style={{margin: '10px 0px'}}>Feel free to explore the features and see how I’ve implemented React concepts in a real-world scenario. If you have any feedback or questions, I’d love to hear from you!</Box>
+
+                        <Box style={{margin: '10px 0px'}}>Enjoy your experience, and thank you again for the opportunity!</Box>
+                        <Box style={{fontWeight: 'bold'}}>Best regards,</Box>
+                        <Box style={{fontWeight: 'bold'}}>Rushikesh Goli</Box>
+                    </Grid>
+
+
+
+
                 </DialogContentText>
                 <Button onClick={onClose} className={classes.activeButton}>Click Here To View Application</Button>
             </DialogContent>
